4acebbe64aca7d320ba9ff95842a97aba311ad3f
m reynard <matheusreynardkolling2@gmail.com>
2026-04-29 13:22:21 -0300

    Update Java version in pom.xml from 25 to 17

---
+++ transenior/src/main/java/com/idoso/uber/domain/model/LoginUser.java
@@ -0,0 +1,39 @@
+package com.idoso.uber.domain.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+
+@Entity
+@Table(name = "login_users")
+public class LoginUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "senha", nullable = false)
+    private String senha;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+}
